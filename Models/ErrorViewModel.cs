dce498d6ee3e0c88e1292aeee69974f0d5de9024
Hermen Tovar <hermendtf@gmail.com>
2023-03-20 08:19:29 -0500

    Creacion estructura MVC del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AplicacionWebProducto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
